5c8fbebb72ff2a62513c0502398a11758e683d02
Cassie Anderson <cbp.smith@gmail.com>
2021-05-17 15:39:01 -0400

    working on validation answer selection when radio button is selected

--- trivia.js
+++ trivia.js
@@ -2,11 +2,13 @@
 
   
   class Game {
-    constructor(players) {
-      this.players = players;
+    constructor(player1, player2) {
+      this.player1 = player1;
+      this.player2 = player2;
     }
 
     start(){
+      this.currentPlayer = this.player1;
         var api_url = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple';
         var config = {
             params: {
@@ -17,10 +19,19 @@
           axios.get(api_url, config)
             .then((response) => {
               this.triviaData = response.data.results;
-              // this.triviaData.forEach(question => console.log(question));
-              this.questions = this.triviaData.forEach(question => console.log(question.question));
-              this.correct_answers = this.triviaData.forEach(question => console.log(question.correct_answer));
-              this.incorrect_answers = this.triviaData.forEach(question => console.log(question.incorrect_answers));
+              var questions = [];
+              var correct_answers = [];
+              var incorrect_answers = [];
+
+              this.triviaData.forEach(question => questions.push(question.question));
+              this.questions = questions;
+
+              this.triviaData.forEach(question => correct_answers.push(question.correct_answer));
+              this.correct_answers = correct_answers;
+
+              this.triviaData.forEach(question => incorrect_answers.push(question.incorrect_answers));
+              this.incorrect_answers = incorrect_answers;
+              
               resolve();
             })
             .catch(function (error) {
@@ -33,12 +44,18 @@
 }
 
   class Question {
-      constructor(answers, correctAns) {
+      constructor(answers, correctAns, question) {
         this.answers = answers;
         this.correctAns = correctAns;
+        this.question = question;
       }
 
       isCorrectAnswer(ans){
+        if(ans === this.correctAns){
+          return "yes";
+        }else{
+          return "no";
+        }
 
       }
   }
@@ -58,16 +75,48 @@
   //easiest way to randomize is have a shuffle routine
 //get user names
 //ask what category and difficulty, pass params to Game
-var triviaGame = new Game(['sally', 'bobby']);
+var triviaGame = new Game('sally', 'bobby');
+// var questionSet;
 triviaGame.start()
   .then(() => {
-    // triviaGame.question;
-    // triviaGame.correct_answer;
-    // triviaGame.incorrect_answers
-    $("#question_display").html(triviaGame.question);//just to test
+    // $("#question_display").html(triviaGame.questions);//working
+
+    //need to add logic to randomize answers
+    for(i=0; i<triviaGame.questions.length; i++){
+      eval('var question'+ i + ' = new Question(triviaGame.incorrect_answers[i],triviaGame.correct_answers[i],triviaGame.questions[i])');
+      $("#question"+i+"question_display").html(triviaGame.questions[i]);
+      $("#question"+i+"answerA_display").html(triviaGame.correct_answers[i]);
+      $("#question"+i+"answerB_display").html(triviaGame.incorrect_answers[i][0]);
+      $("#question"+i+"answerC_display").html(triviaGame.incorrect_answers[i][1]);
+      $("#question"+i+"answerD_display").html(triviaGame.incorrect_answers[i][2]);
+
+    } 
   })
   .catch(() => {});
 
-// numbers.map( (number)=>{
-//   return number * 2
-// } )
+
+function checkIndivAnswer(question, answer){
+  var answerData = $(answer).html;
+  console.log(question);
+  // var isCorrectResult = question.isCorrectAnswer(answerData);
+  var isCorrectResult;
+  this.isCorrectAnswer = isCorrectAnswer(answerData);
+  isCorrectResult = this.isCorrectAnswer;
+  if (isCorrectResult === 'yes'){
+    //current player ++;
+  } 
+}
+
+
+function checkPlayerAnswers(event){
+  event.preventDefault();
+ //  document.querySelectorAll('#triviaGameQuestions input');
+ //clear all
+
+ //switch players
+ // triviaGame.currentPlayer = player2;
+}
+
+// $(question1answerA_display).click(function(){
+
+// });
